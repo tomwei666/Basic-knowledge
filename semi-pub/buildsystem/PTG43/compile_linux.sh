8180042cf9b293f01d3f9aa7fef95d0316507558
tom_wei <tom_wei_666@163.com>
2024-04-13 10:51:08 +0800

    Update once!

--- semi-pub/buildsystem/PTG43/compile_linux.sh
+++ semi-pub/buildsystem/PTG43/compile_linux.sh
@@ -7,7 +7,7 @@ KERNEL_OUT=${CUR_PATH}/out_kernel
 KERNEL_CROSS_COMPILE=${CUR_PATH}/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-androidkernel-
 KERNEL_MAKE_TOOL=${CUR_PATH}/prebuilts/build-tools/linux-x86/bin/make
 PATH_LZ4=${CUR_PATH}/prebuilts/misc/linux-x86/lz4
-PATH_CLANG=/work1/PTG-4.3/buildsystem/android10/prebuilts/clang/host/linux-x86/clang-r349610/bin
+PATH_CLANG=${CUR_PATH}/prebuilts/clang/host/linux-x86/clang-r349610/bin
 
 echo ${KERNEL_PATH}
 echo ${KERNEL_OUT}
@@ -24,14 +24,14 @@ PATH=/usr/bin:$PATH ${KERNEL_MAKE_TOOL} -C kernel mrproper -j8
 # 3. compile Image
 echo "compile Image"
 cd ${KERNEL_PATH}
-#PATH=${PATH_LZ4}:${PATH_CLANG}:/usr/bin:${PATH} CCACHE_NODIRECT=true ${KERNEL_MAKE_TOOL} \
-#O=${KERNEL_OUT} ARCH=${KERNEL_ARCH} CROSS_COMPILE=${KERNEL_CROSS_COMPILE} CC=clang \
-#KCFLAGS="-Wno-incompatible-pointer-types -Wno-parentheses-equality" KAFLAGS="" -j8 Image
+PATH=${PATH_LZ4}:${PATH_CLANG}:/usr/bin:${PATH} CCACHE_NODIRECT=true ${KERNEL_MAKE_TOOL} \
+O=${KERNEL_OUT} ARCH=${KERNEL_ARCH} CROSS_COMPILE=${KERNEL_CROSS_COMPILE} CC=clang \
+KCFLAGS="-Wno-incompatible-pointer-types -Wno-parentheses-equality" KAFLAGS="" -j8 Image
 
 # 3.1 generate the compile json
-PATH=${PATH_LZ4}:${PATH_CLANG}:/usr/bin:${PATH} CCACHE_NODIRECT=true
-bear make CCACHE_NODIRECT=true O=${KERNEL_OUT} ARCH=${KERNEL_ARCH} CROSS_COMPILE=${KERNEL_CROSS_COMPILE} CC=clang \
-KCFLAGS="-Wno-incompatible-pointer-types -Wno-parentheses-equality" KAFLAGS=""
+#PATH=${PATH_LZ4}:${PATH_CLANG}:/usr/bin:${PATH} CCACHE_NODIRECT=true \
+#bear make CCACHE_NODIRECT=true O=${KERNEL_OUT} ARCH=${KERNEL_ARCH} CROSS_COMPILE=${KERNEL_CROSS_COMPILE} CC=clang \
+#KCFLAGS="-Wno-incompatible-pointer-types -Wno-parentheses-equality" KAFLAGS=""
 
 # 4. compile modules
 
